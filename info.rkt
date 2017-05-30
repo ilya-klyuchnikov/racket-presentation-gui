ed219ce7357cf5e959628aaf2bc60d58f8fc6191
Ilya Klyuchnikov <ilyaklyuchnikov@fb.com>
2017-05-30 23:41:51 +0100

    simpler

--- info.rkt
+++
@@ -1,9 +0,0 @@
-#lang info
-(define collection "presentations")
-(define deps '("base"
-               "rackunit-lib"))
-(define build-deps '("scribble-lib" "racket-doc"))
-(define scribblings '(("scribblings/presentations.scrbl" ())))
-(define pkg-desc "Presentation-based GUI widgets")
-(define version "0.0")
-(define pkg-authors '("David Christiansen"))
